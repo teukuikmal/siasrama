bd93fe0afca07edb7a1d0d93bcca9a936fa08e3b
Teuku Ikmal <98931566+teukuikmal@users.noreply.github.com>
2024-01-25 10:18:43 +0700

    Perubahan Ke 2

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "db_siasrama";
+
+$con = mysqli_connect($host,$username,$password,$database);
+
+
+?> 
---
+++ pendaftaran.php
@@ -0,0 +1,15 @@
+<!-- Content Header (Page header) -->
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Dashboard Admin</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
